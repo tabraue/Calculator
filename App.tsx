ead1a8bd9fb6e99264326a3086f02751b0b79db8
Diana Tabraue <dianatabraue@gmail.com>
2023-10-18 10:47:42 +0100

    project initialized

---
+++ App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import CalculatorScreen from './src/screens/CalculatorScreen';
+
+const App = () => {
+  return (
+    <CalculatorScreen/>
+  )
+}
+
+export default App;
---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+
+const CalculatorScreen = () => {
+  return (
+    <View>
+        <Text>CalculatorScreen</Text>
+    </View>
+  )
+}
+
+export default CalculatorScreen
+
+
+const styles = StyleSheet.create({
+    
+});
